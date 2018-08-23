21de99c9793e8c2e331a7c6a5e846c5a2c0a5ee6
shacharm2 <shacharm@gmail.com>
2018-08-23 20:10:36 +0000

    first model

--- model.py
+++ model.py
@@ -1,6 +1,12 @@
+"""
+
+
+
+"""
+
 import data_server
 import tensorflow as tf
-from keras.models import Model, Sequential
+from keras.models import Model, Sequential, load_model
 from keras.layers import Input, Dense, Cropping2D, Lambda, Conv2D, Flatten
 from keras import backend
 from matplotlib import pyplot as plt
@@ -9,7 +15,7 @@ import ipdb
 
 # data_generator = data_server.generate()
 
-def imshow_cropped(image):
+def imshow_cropped(image, save=False, show=False):
 
 	model = Sequential()
 	model.add(Cropping2D(cropping=((50,25), (0,0)), input_shape=image.shape))
@@ -17,9 +23,15 @@ def imshow_cropped(image):
 	cropped_output = backend.function([model.layers[0].input], [model.layers[0].output])
 	new_image = cropped_output([image[None,...]])[0]
 
-	plt.imshow(new_image[0,...]/255, cmap='gray')
-	plt.show()
+	if save:
+		plt.savefig('cropped.png', bbox_inches='tight')
 
+	if show:
+		plt.imshow(new_image[0,...]/255, cmap='gray')
+		plt.show()
+
+
+#def Nvidia
 
 def generate_model(in_shape, show=False):
 	#model = Sequential()
@@ -42,15 +54,6 @@ def generate_model(in_shape, show=False):
 	model.compile(loss="mse", optimizer="adam")
 	return model
 
-	
-
-
-
-
-
-
-	return model
-
 if __name__ == "__main__":
 
 	BATCH_SIZE = 128
@@ -62,11 +65,11 @@ if __name__ == "__main__":
 	# train_generator = data_server.batch_generator(train_type='train', batch_size=BATCH_SIZE)
 	# imshow_cropped(batch_x[0])
 	# ipdb.set_trace()
-	train_generator = data_gen = data_server.DataGenerator("train", batch_size=BATCH_SIZE, shuffle=True)
-	valid_generator = data_gen = data_server.DataGenerator("valid", batch_size=BATCH_SIZE, shuffle=True)
-	EPOCHS=2
+	train_generator = data_server.DataGenerator("train", batch_size=BATCH_SIZE, shuffle=True)
+	valid_generator = data_server.DataGenerator("valid", batch_size=BATCH_SIZE, shuffle=True)
+	EPOCHS=3
 	# model.fit_generator(generator(features, labels, batch_size), samples_per_epoch=50, nb_epoch=10)
-	samples_per_epoch = data_server.Process().samples_per_epoch(batch_size=BATCH_SIZE)
+	# samples_per_epoch = data_server.Process().samples_per_epoch(batch_size=BATCH_SIZE)
 	validation_steps = np.ceil(data_server.Process().total_samples("valid") / BATCH_SIZE)
 	model = generate_model(in_shape=(160, 320, 3))
 
@@ -75,5 +78,13 @@ if __name__ == "__main__":
 		verbose=1,
 		validation_data=valid_generator,
 		validation_steps=validation_steps)
+
+	model.save('model.h5')  # creates a HDF5 file 'my_model.h5'
+	model.save_weights('model_weights.h5')
+
+	del model  # deletes the existing model
+
+	model = load_model('model.h5')
+	# model.load_weights('my_model_weights.h5', by_name=True)
 
 	ipdb.set_trace()
